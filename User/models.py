65688667b06518ef3262e95bc5378ffc13dea1d6
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-02-16 15:14:30 +0530

    added UserCourses model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class UserCourses(models.Model):
+    """
+    UserCourses model
+    """
+
+    ucid = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete="cascade")
+    course_id = models.BigIntegerField()
+    date_created = models.DateTimeField()
+    date_modified = models.DateTimeField()
+
+    def __str__(self):
+        """
+        string representation of the model
+        :return:
+        """
+        return "UserCourses Model: {}, {}" .format(self.user, self.course_id)
+
+    def __repr__(self):
+        """
+        representation of the model
+        :return:
+        """
+        return "UserCourses Model: {}, {}" .format(self.user, self.course_id)
+
