2d460dfc4aa4e3c9e8d0f3856372ab25b9b09676
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-02-16 15:34:07 +0530

    UserTable Model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """ User Table for User Service"""
+    Uid = models.BigAutoField(primary_key=True)
+    FirstNamme = models.CharField(max_length = 20)
+    LastName = models.CharField(max_length = 20)
+    EmailId = models.EmailField(unique = True)
+    Password = models.CharField(max_length = 256)
+    MobileNumber = models.CharField(max_length= 20)
+    DateCreated = models.DateTimeField()
+    DateModified = models.DateTimeField()
+
+    """String Function for All CharField in User Model"""
+    def __str__(self):
+        return "{0}|{1}".format(self.FirstName,self.LastName)
