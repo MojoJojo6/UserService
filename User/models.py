77eaef6ae24a53d00cc44818ebb76bfa56bb2019
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-02-16 18:22:29 +0530

    partial auth development

--- User/models.py
+++ User/models.py
@@ -1,28 +1,87 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,BaseUserManager
+)
+class UserManager(BaseUserManager):
+    def create_user(self,email_id,first_name = None,last_name = None,role = None,password=None,is_staff = False,is_admin = True,is_active = True):
+        if not email_id:
+            raise ValueError("Email Id is required")
+        if not password:
+            raise ValueError("Password is required")
+        if not first_name:
+            raise ValueError("First Name is required")
+        if not last_name:
+            raise ValueError("Last Name is required")
+        if role is None:
+            raise ValueError("Role is required")
 
+        user_obj = self.model(
+            email_id = self.normalize_email(email_id)
+        )
+        user_obj.first_name = first_name
+        user_obj.last_name = last_name
+        user_obj.role = role
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.set_password(password)
+        user_obj.save(using=self.db)
+        return user_obj
+    def create_staffuser(self,email_id,first_name = None,last_name = None,role = None,password = None):
+        user = self.create_user(
+            email_id,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+            role = role,
+            is_staff = True
+        )
+        return user
+    def create_superuser(self,email_id,first_name = None,last_name = None,role = None,password = None):
+        user = self.create_user(
+            email_id,
+            password = password,
+            first_name=first_name,
+            last_name=last_name,
+            role=role,
+            is_staff=True,
+            is_admin= True
+        )
 # Create your models here.
 class User(AbstractBaseUser):
     """ User Table for User Service"""
-    Uid = models.BigAutoField(primary_key=True)
-    FirstNamme = models.CharField(max_length = 20)
-    LastName = models.CharField(max_length = 20)
-    EmailId = models.EmailField(unique = True)
-    Password = models.CharField(max_length = 256)
-    MobileNumber = models.CharField(max_length= 20)
-    Role = models.IntegerField(choices=[(0,"Admin"),(1,"Faculty"),(2,"Student")])
-    is_active = models.BooleanField()
-    DateCreated = models.DateTimeField()
-    DateModified = models.DateTimeField()
+    u_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    email_id = models.EmailField(max_length=255,unique = True)
+    mobile_number = models.CharField(max_length= 20)
+    role = models.IntegerField(choices=[(0,"Admin"),(1,"Faculty"),(2,"Student")])
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default = False)
+    admin = models.BooleanField(default = False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'EmailId'
-    REQUIRED_FIELDS = ['FirstName','LastName','EmailId','Password','MobileNumber','Role']
+    USERNAME_FIELD = 'email_id'
+    REQUIRED_FIELDS = ['first_name','last_name','role']
 
-    def create_user(self):
-        
+    object = UserManager()
+    def get_full_name(self):
+        return "{0} {1}".format(self.first_name,self.last_name)
+    def get_short_name(self):
+        return self.first_name
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
     """String Function for All CharField in User Model"""
     def __str__(self):
-        return "{0}|{1}".format(self.FirstName,self.LastName)
+        return self.email_id
 
 
 
