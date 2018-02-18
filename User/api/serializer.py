6ed9f8f0b3c269be1875c6ba2eab8dfd0e4f15b6
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-02-18 21:24:14 +0530

    serializer added

---
+++ User/api/serializer.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from ..models import User
+from ..models import UserCourses
+from ..models import FacultyCourses
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    serializer for user model
+    """
+    id = serializers.IntegerField(required=False, read_only=True)
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=20)
+    email_id = serializers.EmailField(max_length=255)
+    mobile_number = serializers.CharField(max_length=20, required=False)
+    role = serializers.IntegerField()
+    active = serializers.IntegerField()
+    staff = serializers.IntegerField()
+    admin = serializers.IntegerField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_modified = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email_id', 'mobile_number', 'role', 'active', 'staff', 'admin']
+
+
+class UserCoursesSerializer(serializers.ModelSerializer):
+    """
+    serializer for UserCourses model
+    """
+    id = serializers.IntegerField(required=False, read_only=True)
+    user = serializers.RelatedField()
+    course_id = serializers.RelatedField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_modified = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = UserCourses
+        fields = ['id', 'user', 'course_id']
+
+
+class FacultyCoursesSerializer(serializers.ModelSerializer):
+    """
+    serializer for FacultyCourses model
+    """
+    id = serializers.IntegerField(required=False, read_only=True)
+    user = serializers.RelatedField()
+    course_id = serializers.RelatedField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_modified = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = FacultyCourses
+        fields = ['id', 'user', 'course_id']
--- User/models.py
+++ User/models.py
@@ -47,11 +47,11 @@ class UserManager(BaseUserManager):
 
         user = self.create_user(
             email_id,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
-            role = role,
-            is_staff = True
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            role=role,
+            is_staff=True
         )
         return user
 
@@ -64,7 +64,7 @@ class UserManager(BaseUserManager):
 
         user = self.create_user(
             email_id,
-            password = password,
+            password=password,
             first_name=first_name,
             last_name=last_name,
             role=role,
@@ -76,14 +76,14 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     """ User Table for User Service"""
     u_id = models.BigAutoField(primary_key=True)
-    first_name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
-    email_id = models.EmailField(max_length=255,unique = True)
-    mobile_number = models.CharField(max_length= 20)
-    role = models.IntegerField(choices=[(0,"Admin"),(1,"Faculty"),(2,"Student")])
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_id = models.EmailField(max_length=255, unique=True)
+    mobile_number = models.CharField(max_length=20)
+    role = models.IntegerField(choices=[(0, "Admin"), (1, "Faculty"), (2, "Student")])
     active = models.BooleanField(default=True)
-    staff = models.BooleanField(default = False)
-    admin = models.BooleanField(default = False)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
@@ -94,11 +94,14 @@ class User(AbstractBaseUser):
 
     def get_full_name(self):
         return "{0} {1}".format(self.first_name,self.last_name)
+
     def get_short_name(self):
         return self.first_name
+
     def has_perm(self, perm, obj=None):
         return True
-    def has_module_perms(self,app_label):
+
+    def has_module_perms(self, app_label):
         return True
 
     @property
@@ -118,8 +121,6 @@ class UserCourses(models.Model):
     """
     UserCourses model
     """
-
-
     ucid = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete="cascade")
     course_id = models.BigIntegerField()
