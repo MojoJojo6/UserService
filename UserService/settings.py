9431f2562de4f5860bdeed02d6341b4476e6a17e
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-02-18 20:48:57 +0530

    mysql database added in settings and configurations updated

--- UserService/settings.py
+++ UserService/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'UserService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CapstoneUserDB',
+        'USER': 'root',
+        'PASSWORD': os.environ['mysql_root_password'],
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
