c9d2e13561c939c1a8da4abac84e694a4c164bf3
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-02-24 23:35:04 +0530

    primarykeyrelated field used to show user field in faculty and user courses tables

--- User/api/serializer.py
+++ User/api/serializer.py
@@ -29,11 +29,11 @@ class UserCoursesSerializer(serializers.ModelSerializer):
     """
     serializer for UserCourses model
     """
-    # id = serializers.IntegerField(required=False, read_only=True)
-    # user = UserSerializer(many=True)
-    # course_id = serializers.IntegerField()
-    # date_created = serializers.DateTimeField(read_only=True)
-    # date_modified = serializers.DateTimeField(read_only=True)
+    id = serializers.IntegerField(required=False, read_only=True)
+    user = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    course_id = serializers.IntegerField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_modified = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = UserCourses
@@ -44,11 +44,11 @@ class FacultyCoursesSerializer(serializers.ModelSerializer):
     """
     serializer for FacultyCourses model
     """
-    # id = serializers.IntegerField(required=False, read_only=True)
-    # user = UserSerializer(many=True)
-    # course_id = serializers.IntegerField()
-    # date_created = serializers.DateTimeField(read_only=True)
-    # date_modified = serializers.DateTimeField(read_only=True)
+    id = serializers.IntegerField(required=False, read_only=True)
+    user = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    course_id = serializers.IntegerField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_modified = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = FacultyCourses
