d85c7ae0fa576086363ca1a687e9214b17ee7120
RiflerRick <rajdeep.mukherjee295@gmail.com>
2018-03-06 17:08:49 +0530

    debugged user creation

--- User/api/serializer.py
+++ User/api/serializer.py
@@ -8,21 +8,23 @@ class UserSerializer(serializers.ModelSerializer):
     """
     serializer for user model
     """
+
     u_id = serializers.IntegerField(required=False, read_only=True)
     first_name = serializers.CharField(max_length=20)
     last_name = serializers.CharField(max_length=20)
     email_id = serializers.EmailField(max_length=255)
+    password = serializers.CharField(max_length=255, write_only=True, required=True)
     mobile_number = serializers.CharField(max_length=20, required=False)
-    role = serializers.IntegerField()
-    active = serializers.IntegerField()
-    staff = serializers.IntegerField()
-    admin = serializers.IntegerField()
+    role = serializers.ChoiceField(choices=User.roles)
+    active = serializers.BooleanField(default=True)
+    staff = serializers.BooleanField(default=False)
+    admin = serializers.BooleanField(default=False)
     date_created = serializers.DateTimeField(read_only=True)
     date_modified = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['u_id', 'first_name', 'last_name', 'email_id', 'mobile_number', 'role', 'active', 'staff', 'admin',
+        fields = ['u_id', 'first_name', 'last_name', 'email_id', 'password', 'mobile_number', 'role', 'active', 'staff', 'admin',
                   'date_created', 'date_modified']
 
 
--- User/models.py
+++ User/models.py
@@ -3,83 +3,45 @@ from django.contrib.auth.models import (
     AbstractBaseUser,BaseUserManager,
 )
 
+
 class UserManager(BaseUserManager):
 
-    def create_user(self,
-                    email_id,
-                    first_name=None,
-                    last_name=None,
-                    role=None,
-                    password=None,
-                    is_staff=False,
-                    is_admin=True,
-                    is_active=True):
+    def create(self, **validated_data):
 
-        if not email_id:
+        if not validated_data["email_id"]:
             raise ValueError("Email Id is required")
-        if not password:
+        if not validated_data["password"]:
             raise ValueError("Password is required")
-        if not first_name:
+        if not validated_data["first_name"]:
             raise ValueError("First Name is required")
-        if not last_name:
+        if not validated_data["last_name"]:
             raise ValueError("Last Name is required")
-        if role is None:
+        if validated_data["role"] is None:
             raise ValueError("Role is required")
 
-        user_obj = self.model( email_id=self.normalize_email(email_id))
-        user_obj.first_name = first_name
-        user_obj.last_name = last_name
-        user_obj.role = role
-        user_obj.staff = is_staff
-        user_obj.admin = is_admin
-        user_obj.active = is_active
-        user_obj.set_password(password)
+        user_obj = self.model( email_id=self.normalize_email(validated_data["email_id"]))
+        user_obj.first_name = validated_data["first_name"]
+        user_obj.last_name = validated_data["last_name"]
+        user_obj.role = validated_data["role"]
+        user_obj.mobile_number = validated_data["mobile_number"]
+        user_obj.staff = validated_data["staff"]
+        user_obj.admin = validated_data["admin"]
+        user_obj.active = validated_data["active"]
+        user_obj.set_password(validated_data["password"])
         user_obj.save(using=self.db)
         return user_obj
-
-    def create_staffuser(self,
-                         email_id,
-                         first_name=None,
-                         last_name=None,
-                         role=None,
-                         password=None):
-
-        user = self.create_user(
-            email_id,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-            role=role,
-            is_staff=True
-        )
-        return user
-
-    def create_superuser(self,
-                         email_id,
-                         first_name=None,
-                         last_name=None,
-                         role=None,
-                         password=None):
-
-        user = self.create_user(
-            email_id,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-            role=role,
-            is_staff=True,
-            is_admin= True
-        )
 
 
 class User(AbstractBaseUser):
     """ User Table for User Service"""
+    roles = [(0, "Admin"), (1, "Faculty"), (2, "Student")]
+
     u_id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email_id = models.EmailField(max_length=255, unique=True)
     mobile_number = models.CharField(max_length=20)
-    role = models.IntegerField(choices=[(0, "Admin"), (1, "Faculty"), (2, "Student")])
+    role = models.IntegerField(choices=roles)
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
