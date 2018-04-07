994c724570d1a74e3818e875510e5aaf0dd54189
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-04-07 16:41:23 +0530

    Custom Permission, Trial

---
+++ User/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+class IsFaculty(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.roles == 1:
+            return True
+        else:
+            return False
